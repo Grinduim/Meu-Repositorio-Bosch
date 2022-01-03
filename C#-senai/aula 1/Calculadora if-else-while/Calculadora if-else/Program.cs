b1f3ca32d42c1e2400229f2238ab1783f2255fb5
vinicius <viniciusgribelperson@gmail.com>
2022-01-03 11:11:30 -0300

    adicionado arquivos primeira aula C#

---
+++ C#-senai/aula 1/Calculadora if-else-while/Calculadora if-else/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Calculadora_if_else
+{
+    class Program
+    {
+
+        public static  void operacoes(int num1, int num2, string opc)
+        {
+            if (opc == "+")
+            {
+                Console.WriteLine($"\n{num1} + {num2} = {num1 + num2}");
+
+            }
+            else if(opc == "-")
+            {
+                Console.WriteLine($"\n{num1} - {num2} = {num1- num2}");
+
+            }
+            else if (opc == "/")
+            {
+                float res = (float)num1 / (float)num2;
+                Console.WriteLine($"\n{num1} / {num2} = { res}");
+            }
+            else if (opc == "*")
+            {
+                Console.WriteLine($"\n{num1} * {num2} = {num1 * num2}");
+            }
+            else
+            {
+                Console.WriteLine("\nNão foi possivel indentificar a operação");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            //Escreva um programa que pergunte ao usuario dois numeros e a operação que ele deseja fazer.
+
+            int num1;
+            int num2;
+            string operacao;
+            int cond = 1;
+
+            while (cond != 0) {
+                Console.WriteLine("\nInsira o primeiro numero ");
+                num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nInsira o segundo numero ");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nQual operação deseja fazer ? + - * /");
+                operacao = Console.ReadLine();
+
+                operacoes(num1, num2, operacao);
+
+                Console.WriteLine("\nDeseja continuar ? se sim, insira qualquer numero, se não insira 0");
+                cond = int.Parse(Console.ReadLine());
+            }
+            
+        }
+    }
+}
---
+++ C#-senai/aula 1/FOR/FOR/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Escreva um programa que receba um numero natural e imprime todos os nmeros pares de 0 até o numero inserido.
+
+
+            int num;
+
+            Console.WriteLine("Insira um numero");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Todos os numeros pares começando do 0 até  {num} que é o numero inserido");
+            for(int i = 0; i < num+1; i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine($"{i}");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
