4357d1460cbeadc060ae3c99ebbe74b8a2aeb5c4
Grinduim <viniciusgribelperson@gmail.com>
2022-01-04 11:13:29 -0300

    Adicionando o inicio do projeto hotel

---
+++ C#-senai/Nova pasta/Try e cath/Try e cath/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_e_cath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Insira o primeiro numero");
+                int n1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Insira o segundo numero");
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Divisão por zero não existe!");
+            }
+            catch(FormatException)
+            {
+               Console.WriteLine("Não é possivel fazer operação aritimética com letras");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);    
+            }
+                           
+        }
+    }
+}
---
+++ C#-senai/aula2/Hotel/Hotel/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> quarto = new List<int>();
+            List<string> entrada = new List<string>();
+            List<string> saida = new List<string>();
+
+            try
+            {
+                DateTime DataAtual = DateTime.Now; // data atual
+                int TempQuarto; // Variaveis temporarias para auxiliarem na validação de itens
+                DateTime TempEntrada;
+                DateTime TempSaida;
+
+                Console.WriteLine("\nInsira o numero do quarto: ");
+                TempQuarto = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\n Qual da Data de entrada ? Formato yyyy/MM/ddd ");
+                TempEntrada = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("\n Qual a data de saida ? Formato yyyy/MM/ddd ");
+                TempSaida = DateTime.Parse(Console.ReadLine());
+
+
+                if (TempEntrada > DataAtual && TempSaida > TempEntrada)
+                {
+                    Console.WriteLine("Validous");
+                }
+
+                // continuar a parte de validação das datas
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Erro: {e}");
+            }
+
+
+            /*Console.WriteLine(DataAtual.ToString("dd/MM/yyyy"));
+            DateTime teste = DateTime.Parse("2009-05-08"); //  string to data 
+            Console.WriteLine(teste.ToString("dd/MM/yyyy"));
+
+            if(DataAtual < teste) // validação de datas 
+            {
+                Console.WriteLine("teste1");
+            }*/
+
+
+
+        }
+    }
+}
+
