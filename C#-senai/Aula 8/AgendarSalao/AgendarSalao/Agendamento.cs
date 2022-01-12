e644179d6c159d0933b8fa49f2e3667c10f77398
Vini <viniciusgribelperson@gmail.com>
2022-01-12 11:06:20 -0300

    upload ReservaSalai

---
+++ C#-senai/Aula 8/AgendarSalao/AgendarSalao/Agendamento.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgendarSalao;
+using System.Globalization;
+
+namespace AgendarSalao
+{
+    class Agendamento
+    {
+        String name;
+        DateTime schedule;
+
+        public Agendamento()
+        {
+             string TempName;
+                DateTime Tempschedule;
+                while (true)
+                {
+
+                    try
+                    {
+
+                        Console.Write("\nInsert the name: ");
+                        TempName = Console.ReadLine();
+
+                        Console.WriteLine("\nInsert a day of the visit\nPlease Format -> dd/MM/yyyy\n");
+                        Tempschedule = DateTime.Parse(Console.ReadLine(), new CultureInfo("pt-BR"));
+
+                        if (Tempschedule > DateTime.Now)
+                        {
+                            Name = TempName;
+                            Schedule = Tempschedule;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"\nDateTime Invalid, please, insert a  a date later than today  -> {DateTime.Now.ToString("dd/MM/yyyy")}");
+                            continue;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+                Console.WriteLine($"\nReservation Confirmed\nName:{Name} and Date: {Schedule}");
+        }
+        public string Name { get => name; set => name = value; }
+        public DateTime Schedule { get => schedule; set => schedule = value; }
+    }
+}
---
+++ C#-senai/Aula 8/AgendarSalao/AgendarSalao/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using AgendarSalao;
+
+namespace AgendarSalao
+{
+    class Program
+    {
+
+         
+        static void Main(string[] args)
+        {
+            string Name;
+            DateTime Schedule;
+            while (true) // make inifnity input in case erro
+            {
+                try
+                {
+
+                    Console.Write("\nInsert the name: ");
+                    Name = Console.ReadLine();
+
+                    Console.WriteLine("\nInsert a day of the visit\nPlease Format -> dd/MM/yyyy\n");
+                    Schedule = DateTime.Parse(Console.ReadLine(), new CultureInfo("pt-BR"));
+
+                    if (Schedule > DateTime.Now)
+                    {
+
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nDateTime Invalid, please, insert a  a date later than today  -> {DateTime.Now.ToString("dd/MM/yyyy")}");
+                        continue;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Erro: please inser a valid format");
+                    continue;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    continue;
+                }
+            }
+            Console.WriteLine($"\nReservation Confirmed\nName:{Name} and Date: {Schedule}");
+            Console.Clear();
+
+            while (true)
+            {
+                DateTime NewSchedule;
+                try
+                {
+                    Console.WriteLine("\nInsert the new day of the visit\nPlease Format -> dd/MM/yyyy\n");
+                    NewSchedule = DateTime.Parse(Console.ReadLine(), new CultureInfo("pt-BR"));
+                    if (NewSchedule > Schedule)
+                    {
+                        Schedule = NewSchedule;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nDateTime Invalid, please, insert a  a date later than older schedule  -> {Schedule.ToString("dd/MM/yyyy")}");
+                        continue;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Erro: please inser a valid format");
+                    continue;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    continue;
+                }
+            }
+
+
+
+
+        }
+    }
+}
