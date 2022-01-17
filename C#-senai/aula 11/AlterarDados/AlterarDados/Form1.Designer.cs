a96b0fd3663485d62acfde2da11da3f27e16c238
vini <viniciusgribelperson@gmail.com>
2022-01-17 11:07:54 -0300

    aaaa

---
+++ C#-senai/aula 11/AlterarDados/AlterarDados/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace AlterarDados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pesquisar = new System.Windows.Forms.Button();
+            this.alterar = new System.Windows.Forms.Button();
+            this.combo = new System.Windows.Forms.ComboBox();
+            this.codigo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cod = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // pesquisar
+            // 
+            this.pesquisar.Location = new System.Drawing.Point(273, 110);
+            this.pesquisar.Name = "pesquisar";
+            this.pesquisar.Size = new System.Drawing.Size(91, 37);
+            this.pesquisar.TabIndex = 2;
+            this.pesquisar.Text = "Pesquisar";
+            this.pesquisar.UseVisualStyleBackColor = true;
+            this.pesquisar.Click += new System.EventHandler(this.pesquisar_Click);
+            // 
+            // alterar
+            // 
+            this.alterar.Location = new System.Drawing.Point(458, 68);
+            this.alterar.Name = "alterar";
+            this.alterar.Size = new System.Drawing.Size(91, 37);
+            this.alterar.TabIndex = 5;
+            this.alterar.Text = "Alterar";
+            this.alterar.UseVisualStyleBackColor = true;
+            this.alterar.Click += new System.EventHandler(this.alterar_Click);
+            // 
+            // combo
+            // 
+            this.combo.FormattingEnabled = true;
+            this.combo.Location = new System.Drawing.Point(92, 119);
+            this.combo.Name = "combo";
+            this.combo.Size = new System.Drawing.Size(121, 21);
+            this.combo.TabIndex = 6;
+            this.combo.SelectedIndexChanged += new System.EventHandler(this.combo_SelectedIndexChanged);
+            // 
+            // codigo
+            // 
+            this.codigo.AutoSize = true;
+            this.codigo.Location = new System.Drawing.Point(34, 55);
+            this.codigo.Name = "codigo";
+            this.codigo.Size = new System.Drawing.Size(40, 13);
+            this.codigo.TabIndex = 0;
+            this.codigo.Text = "Codigo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome";
+            // 
+            // cod
+            // 
+            this.cod.Location = new System.Drawing.Point(92, 56);
+            this.cod.Name = "cod";
+            this.cod.Size = new System.Drawing.Size(100, 20);
+            this.cod.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.combo);
+            this.Controls.Add(this.alterar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pesquisar);
+            this.Controls.Add(this.cod);
+            this.Controls.Add(this.codigo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button pesquisar;
+        private System.Windows.Forms.Button alterar;
+        private System.Windows.Forms.ComboBox combo;
+        private System.Windows.Forms.Label codigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox cod;
+    }
+}
+
---
+++ C#-senai/aula 11/AlterarDados/AlterarDados/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AlterarDados
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection Connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CarregaBanco();
+        }
+
+        private void pesquisar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data_source = "datasource=localhost;username=root;password=usbw;database=teste"; // data source 
+
+                Connection = new MySqlConnection(data_source); // conbeção
+
+                string sql = $"SELECT cod, nome FROM teste1 WHERE nome = {combo.Text}"; // select do item de acordo com o q foi inserido
+
+                MySqlCommand command = new MySqlCommand(sql, Connection); // dita os dados da conexao
+
+                Connection.Open(); // abre coneção
+
+                MySqlDataReader data = command.ExecuteReader(); // armazena todos os dados do selecet e executa ele
+
+                if (data.Read())
+                {
+
+                    cod.Text = data.GetString(1);
+                }
+                else
+                {
+                    MessageBox.Show("Registro não encontrado");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Connection.Close();
+            }
+
+        }
+
+        private void alterar_Click(object sender, EventArgs e)
+        {
+            string data_source = "datasource=localhost;username=root;password=usbw;database=teste";
+
+            Connection = new MySqlConnection(data_source);
+
+            string sql = $"UPDATE teste1 set nome = {combo.Text} where cod = {cod.Text}";
+
+            MySqlCommand command = new MySqlCommand(sql, Connection);   
+        }
+
+        private void combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+         void CarregaBanco () // função para carregar dados do banco
+        {
+            try
+            {
+
+                string data_source = "datasource=localhost;username=root;password=usbw;database=teste"; // data source do banco
+                Connection = new MySqlConnection(data_source); // conexão
+
+                string querynome = $"SELECT NOME FROM TESTE1"; // querry
+
+                MySqlCommand command = new MySqlCommand(querynome, Connection);
+
+                Connection.Open();
+
+                MySqlDataReader myReader = command.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    string sNome = myReader.GetString("nome"); // lendo os nomes e adicionando eles an coleção do combo box
+                    combo.Items.Add(sNome);
+                }
+
+                MessageBox.Show("Banco carregou");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Connection.Close();
+            }
+
+        }
+    }
+}
