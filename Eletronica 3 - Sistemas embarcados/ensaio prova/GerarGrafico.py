70ec653d0f4eb530798e6da9f322478707981bb2
Sala Digital (CtP/ETS) <disrct@bosch.com>
2022-01-27 16:41:30 -0300

    upload arquivos

--- Eletronica 3 - Sistemas embarcados/ensaio prova/GerarGrafico.py
+++ Eletronica 3 - Sistemas embarcados/ensaio prova/GerarGrafico.py
@@ -11,18 +11,27 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 
+#dados de conexão com o sql
 server = 
 database =
 username = 
 password = 
 driver= '{ODBC Driver 17 for SQL Server}'
 conn= pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor=conn.cursor()
-cursor.execute("SELECT Luminosidade, timestamp, FROM table")
-row = cursor.fetchone();
+cursor=conn.cursor() 
+cursor.execute("SELECT Luminosidade, timestamp, FROM table") #executando comando sql
+row = cursor.fetchone(); #pegando a primeira linha
 
 listaLuminosidade = []
 listaTempo = []
 
 while row:
     listaLuminosidade.append(row[0])
+    listaTempo.append(row[1])
+    row = cursor.fetchone() #pegando a proxima linha
+    
+df = pd.DataFrame({"Luminosidae":listaLuminosidade,"Tempo":listaTempo})
+df["Tempo"] = pd.to_datetime(df["Tempo"])
+sns.relplot(x="Luminosidade",y="Tempo",data=df,kind = line)
+plt.title("Luminosidade em função do tempo")
+plt.xticks(rotation= 90)
